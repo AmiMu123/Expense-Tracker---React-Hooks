fc1915a26f51ac03acad3c0fb4af5358e835fe3e
Amir M <user@Amir.local>
2020-04-07 07:31:37 +0300

    expense tracker with react hooks

---
+++ src/context/GlobalState.js
@@ -0,0 +1,43 @@
+
+import React , {createContext, useReducer} from 'react';
+import AppReducer from './AppReducer' ;
+// initial state
+const initialState = {
+    transactions:[]
+    }
+ // create context
+    export const GlobalContext = createContext(initialState);    
+
+    
+
+// provider component
+export const GlobalProvider = ({children}) =>{
+    const [state,dispatch]  = useReducer(AppReducer,initialState);
+
+    // actions
+    const deleteTransaction=(id)=> 
+        dispatch({
+        type :'delete_transaction',
+        payload : id
+    })
+
+//     const addTransaction=(transaction)=> 
+//     dispatch({
+//     type :'add_transaction',
+//     payload : transaction
+// })
+function addTransaction(transaction){
+    dispatch({
+        type : 'add_transaction',
+        payload : transaction
+    })
+}
+
+
+    return (<GlobalContext.Provider
+    value ={{
+        transactions:state.transactions,
+        deleteTransaction,
+        addTransaction
+    }}>{children}</GlobalContext.Provider>)
+} 
